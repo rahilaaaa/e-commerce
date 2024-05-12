1b01c5abb5dfbe72e633410898172154ee5a8537
mkrahila117@gmail.com <mkrahila117@gmail.com>
2024-05-12 20:27:04 +0530

    featured product& latest product

--- ecom/products/views.py
+++ ecom/products/views.py
@@ -4,14 +4,22 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    featured_products = Product.objects.order_by('priority')[:4]
+    latest_products = Product.objects.order_by('-id')[:4]
+    context = {
+        'featured_products':featured_products,
+        'latest_products':latest_products
+    }
+
+
+    return render(request,'index.html',context)
 
 
 def Product_list(request):
     page = 1
     if request.GET:
         page = request.GET.get('page',1)
-    list_product = Product.objects.order_by('-  priority')
+    list_product = Product.objects.order_by('-priority')
     product_paginator = Paginator(list_product,10)
     list_product = product_paginator.get_page(page)
     context = {'products':list_product}
