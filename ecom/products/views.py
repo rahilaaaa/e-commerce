f4b4666e2c8b48147f35144fbdda112b5ae841fc
mkrahila117@gmail.com <mkrahila117@gmail.com>
2024-05-12 11:48:21 +0530

    product manage

--- ecom/products/views.py
+++ ecom/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from . models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
@@ -6,7 +8,14 @@ def index(request):
 
 
 def Product_list(request):
-    return render(request,'product_layout.html')
+    page = 1
+    if request.GET:
+        page = request.GET.get('page',1)
+    list_product = Product.objects.all()
+    product_paginator = Paginator(list_product,10)
+    list_product = product_paginator.get_page(page)
+    context = {'products':list_product}
+    return render(request,'product_layout.html',context)
 
 def Detail_product(request):
     return render(request,'product_detail.html')
